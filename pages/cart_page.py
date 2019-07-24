9074ef2e62723274775a22867a9d9ea32dbb1e7d
Evgeny Novashov <Evgeny.Novashov@evraz.com>
2019-07-25 01:05:11 +0700

    final code for review

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -7,6 +7,6 @@ class CartPage(BasePage):
 
     def should_be_message_basket_empty(self):
         basket_message = self.browser.find_element(*BasketPageLocators.BASKET_MESSAGE_EMPTY).text
-        assert basket_message == 'Ваша корзина пуста Продолжить покупки', "Basket message is not empty"
+        assert basket_message == 'Your basket is empty. Continue shopping', "Basket message is not empty"
 
 
